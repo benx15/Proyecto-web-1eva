1b6c032dd0b85789e5ad8e17f0932f444cdcd38d
David-Castr0 <davecr1313@gmail.com>
2025-11-17 14:19:24 +0100

    pagina admin-principal y usuarios

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ import { Register } from './app/register/register';
 import { Login } from './app/login/login';
 import { Component } from '@angular/core';
 import { appConfig } from './app/app.config';
+import { AdminLogin } from './app/admin-login/admin-login';
+import { AdminPrincipal } from './app/admin-principal/admin-principal';
+
 
 const routes = [
   { path: '', component: Principal },
   { path: 'register', component: Register },
   { path: 'login', component: Login},
+  { path: 'admin-principal', component: AdminPrincipal},
 ];
 
 bootstrapApplication(App, appConfig)
